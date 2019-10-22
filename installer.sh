dfcbe71c8d51d832a8e6b50cbf892d2180b5165a
2nd-junkey <broken.phantsm@gmail.com>
2019-10-22 15:24:01 +0900

    .sshで検証

--- installer.sh
+++ installer.sh
@@ -1,9 +1,26 @@
-#!/usr/bash
+#!/usr/bin/bash
+cd `dirname $0`
 
 # googleドライブの場所を確認
+# TODO: 複数アカウントの場合どうなるか
+DRIVE=`find /run/user/ -type d -name "google-drive*"`
+body="$DRIVE/restarter"
 
-# 既に登録してあった場合、そのデータを使ってレストアするか上書きするかの確認
+# TODO: 既に登録してあった場合、そのデータを使ってレストアするか上書きするかの確認
+if [ -d $body ] then
+    rm -fr $body
+fi
 
 # 初期登録として上書き処理
+cp -r body/ $body
 
-# シンボリックリンクの作成、環境変数に追加
+# シンボリックリンクの作成
+# TODO: 他のも移行できるように
+mkdir -p $body/link
+cp -r ~/.ssh $body/link/.ssh
+rm -fr ~/.ssh
+ln -s $body/link/.ssh ~/.ssh
+
+# 環境変数の追加と更新
+echo "export PATH=$body:\$PATH">>~/.zshrc
+source ~/.zshrc
